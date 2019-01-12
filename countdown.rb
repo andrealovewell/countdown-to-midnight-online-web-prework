35d50038b0f4d779d0a5b6bbf23f84b9d77b8cad
Andrea Lovewell <andy.lovewell@gmail.com>
2019-01-12 23:17:09 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
-
-def countdown
+seconds = 10
+def countdown (seconds)
+  while(seconds !=0)
+    puts "#{seconds} Second(S)!"
+    seconds -= 1
+    sleep(5)
+  end
+  if (seconds == 0)
+    puts "Happy New Year"
+  end
 end
