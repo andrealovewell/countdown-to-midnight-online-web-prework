3e5ddc6f2cce24aa96c654ef8f867720fd534e75
Andrea Lovewell <andy.lovewell@gmail.com>
2019-01-16 16:07:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,12 @@
-#write your code here
+
 seconds = 10
 def countdown (seconds)
   while(seconds !=0)
-    puts "#{seconds} Second(S)!"
+    puts "#{seconds} SECOND(S)!"
     seconds = seconds - 1
-    sleep(5)
+    sleep(.5)
   end
   if (seconds == 0)
-    return "Happy New Year"
+    return "HAPPY NEW YEAR!"
   end
 end
