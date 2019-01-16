c80d4f767e3807300b44993d50b6e1eec104bacd
Andrea Lovewell <andy.lovewell@gmail.com>
2019-01-16 16:12:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown (seconds)
   while(seconds !=0)
     puts "#{seconds} SECOND(S)!"
     seconds = seconds - 1
-    sleep(0.46)
+    sleep(0.45)
   end
   if (seconds == 0)
     return "HAPPY NEW YEAR!"
